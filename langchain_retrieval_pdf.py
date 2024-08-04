1d1ab20b8f62c408c8d440ab57ef25f110a91c08
José Netto <dev.jnetto@gmail.com>
2024-08-04 12:17:53 -0300

    teste

--- langchain_retrieval_pdf.py
+++ langchain_retrieval_pdf.py
@@ -1,29 +1,29 @@
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.prompts import ChatPromptTemplate
-from langchain.chains import SimpleSequentialChain
-from langchain.chains import LLMChain
-from langchain.chains import ConversationChain
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
+from langchain.chains import SimpleSequentialChain, LLMChain, ConversationChain, RetrievalQA
 from langchain.globals import set_debug
-from langchain_openai import OpenAIEmbeddings
 from langchain.memory import ConversationSummaryMemory
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
-from langchain_text_splitters import CharacterTextSplitter
-import os
+from langchain.text_splitters import CharacterTextSplitter
 from langchain_core.output_parsers import StrOutputParser
-from langchain.chains import RetrievalQA
 
-from dotenv import load_dotenv
-
+# Carregar variáveis de ambiente
 load_dotenv()
+
+# Habilitar debug
 set_debug(True)
 
+# Configurar o modelo de linguagem
 llm = ChatOpenAI(
     model="gpt-3.5-turbo",
     temperature=0.5,
-    api_key=os.getenv("OPENAI_API_KEY"))
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
+# Carregar documentos PDF
 carregadores = [
     PyPDFLoader("GTB_standard_Nov23.pdf"),
     PyPDFLoader("GTB_gold_Nov23.pdf"),
@@ -34,16 +34,18 @@ documentos = []
 for carregador in carregadores:
     documentos.extend(carregador.load())
 
-
+# Dividir documentos em partes menores
 quebrador = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 textos = quebrador.split_documents(documentos)
-# print(textos)
 
+# Criar embeddings para os textos
 embeddings = OpenAIEmbeddings()
 db = FAISS.from_documents(textos, embeddings)
 
+# Configurar a cadeia de perguntas e respostas
 qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
 
-pergunta = "Como devo proceder caso tenha um item comprado roubado"
-resultado = qa_chain.invoke({ "query" : pergunta})
+# Fazer uma pergunta ao modelo
+pergunta = "Como devo proceder caso tenha um item comprado roubado?"
+resultado = qa_chain.invoke({"query": pergunta})
 print(resultado)
